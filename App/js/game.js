b9cd5b3bdafa12ec6de302f1dc26ea0fb96ded09
je222ks <je222ks@student.lnu.se>
2014-05-26 21:44:38 +0200

    basic rendering

---
+++ App/js/game.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var Background = function () {
+    var canvas = document.getElementById("gamecont");
+    var ctx = canvas.getContext("2d");
+    var cols = 30;
+    var rows = 20;
+    var tileSize = 20;
+    
+    
+    var map = [
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    ];
+    
+    canvas.width = tileSize*cols;
+    canvas.height = tileSize*rows;
+        
+    renderMap();
+        
+    function renderMap () {
+        var tilesetImage = new Image();
+        tilesetImage.src = "../img/tiles.png";
+        tilesetImage.onload = drawImage;
+        var tileSize = 20;       // The size of a tile (20×20)
+        var rowTileCount = 20;   // The number of tiles in a row 
+        var colTileCount = 30;   // The number of tiles in a column
+        var imageNumTiles = 5;  // The number of tiles per row in the tileset image
+        function drawImage () {
+           for (var r = 0; r < rowTileCount; r++) {
+              for (var c = 0; c < colTileCount; c++) {
+                 var tile = map[ r ][ c ];
+                 var tileRow = (tile / imageNumTiles) | 0;
+                 var tileCol = (tile % imageNumTiles) | 0;
+                 ctx.drawImage(tilesetImage, (tileCol * tileSize), (tileRow * tileSize), tileSize, tileSize, (c * tileSize), (r * tileSize), tileSize, tileSize);
+              }
+           }
+        }
+    };    
+};
+
+
+
+          
+window.onload = Background;
